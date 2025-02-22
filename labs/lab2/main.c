8552b2a2ac674997410971f42c8c148a633b07a5
Escalipt <108051746+Escalipt@users.noreply.github.com>
2025-02-22 12:56:50 +1000

    Update main.c

--- labs/lab2/main.c
+++ labs/lab2/main.c
@@ -1,35 +1,20 @@
-#include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdio.h>
 
 int main() {
-	double mat [3][3]={
-		{1,2,3},
-		{4,5,6},
-		{7,8,9}	
-	};
-	
-	int a,b;
-	for (a=0,a<3,a++){
-		for (b=0,a<3, a++){
-			printf("%lf\t",mat[a][b],"/n");
-			
-		
-		
-		}
-	    
-	    
-	    
-	    	printf("\n");
-		
-		
-	}
-	
-	
-	
-	
-   
-   
-   
-	return 0;
+    double mat[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int a, b;
+    for (a = 0; a < 3; a++) {
+        for (b = 0; b < 3; b++) {
+            printf("%lf\t", mat[a][b]);
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
 }
